bd659ac9230af268957303ceccb6ee6a9af4b43d
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-03-18 16:45:17 -0700

    Finish base features

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -7,5 +7,20 @@
 
   Chat.prototype.sendMessage = function(message) {
     this.socket.emit("sendMessage", { message: message });
-  }
+  };
+
+  Chat.prototype.handleCommand = function(message) {
+
+    if (message.match("^/nick"  )) {
+      var newNickname = message.substr(6);
+      this.socket.emit("nicknameChangeRequest", { newNickname: newNickname });
+    } else if (message.match("^/join")) {
+      var newRoom = message.substr(6);
+      this.socket.emit("handleRoomChangeRequest", { room: newRoom });
+    } else {
+      this.sendMessage(message);
+    }
+  };
+
+
 }(this));
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -8,13 +8,38 @@ $(function() {
     event.preventDefault();
 
     var $message = $("input[type='text']").val();
-    chat.sendMessage($message);
+    var nameChangeCommand = new RegExp("^/");
+
+    if ($message.match(nameChangeCommand)) {
+      chat.handleCommand($message)
+    } else {
+      chat.sendMessage($message);
+    }
     $("input[type='text']").val("");
+  });
+
+  socket.on("nicknameChangeResult", function(data) {
+    $('#username').html("Your nickname: " + data.newNickname);
+  })
+
+  socket.on("roomChangeResult", function(data) {
+    $("#current-room").html("You're currently in: " + data.room);
+    $("#room-members").html("");
+    data.members.forEach(function(member) {
+      $("#room-members").append("<li>"+ member + "</li>")
+    })
   });
 
   socket.on("postMessage", function(data) {
     console.log("POSTMESSAGE")
-    var message = data.message;
-    $("#messages-board").append("<p>" + message + "</p>");
-  })
+    var text = data.text;
+    $("#messages-board").append("<p>" + text + "</p>");
+  });
+
+  socket.on("roomListUpdate", function(data) {
+    $("#room-members").html("");
+    data.members.forEach(function(member) {
+      $("#room-members").append("<li>"+ member + "</li>")
+    })
+  });
 });
